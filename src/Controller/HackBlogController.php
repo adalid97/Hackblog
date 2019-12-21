43c42dbd07504323382ca001af3c0c0e8c3e3f29
adalid97 <jadalid97@gmail.com>
2019-12-21 12:34:45 +0100

    Formulario

--- src/Controller/HackBlogController.php
+++ src/Controller/HackBlogController.php
@@ -2,6 +2,13 @@
 // Controller del HackBlog
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Comentario;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
 class HackBlogController extends AbstractController
 {
     public function portada()
@@ -14,4 +21,21 @@ class HackBlogController extends AbstractController
         return $this->render('noticia.html.twig', [    
         ]);
     }
+
+    public function nuevoComentario(Request $request)
+    {
+        $comentario = new Comentario();
+
+        $form = $this->createFormBuilder($comentario)
+        ->add('nombre', TextType::class)
+        ->add('comentario', TextType::class)
+        ->add('save', SubmitType::class,
+        array('label' => 'Añadir Comentario'))
+        ->getForm();
+        return $this->render('noticia.html.twig', array(
+        'form' => $form->createView(),
+        ));
+    }
+
+
 }
---
+++ src/Entity/Comentario.php
@@ -0,0 +1,28 @@
+<?php
+// src/Entity/Comentario.php
+namespace App\Entity;
+class Comentario
+{
+    protected $nombre;
+    protected $comentario;
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getComentario()
+    {
+        return $this->comentario;
+    }
+
+    public function setComentario($comentario)
+    {
+        $this->comentario = $comentario;
+    }
+}

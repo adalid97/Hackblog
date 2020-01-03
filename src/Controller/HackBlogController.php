f7660ff3ba3a1ac203b49fce8b841bea3a4bd77f
adalid97 <jadalid97@gmail.com>
2020-01-03 11:50:32 +0100

    Cambiado el tipo de entradilla y cuerpo.  TextType por TextareaType.

--- src/Controller/HackBlogController.php
+++ src/Controller/HackBlogController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Noticia;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -28,8 +29,8 @@ class HackBlogController extends AbstractController
 
         $form = $this->createFormBuilder($noticia)
         ->add('titular', TextType::class)
-        ->add('entradilla', TextType::class)
-        ->add('cuerpo', TextType::class)
+        ->add('entradilla', TextareaType::class)
+        ->add('cuerpo', TextareaType::class)
         ->add('fecha', DateType::class)
         ->add('save', SubmitType::class,
         array('label' => 'Añadir Noticia'))
